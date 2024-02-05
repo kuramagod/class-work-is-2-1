3d75d7c82434e7f7e1351c75e1c3b076d8133cf7
kuramagod <raxutogame@gmail.com>
2024-02-05 14:49:29 +0300

    автоматизировал код

--- task_3/main.js
+++ task_3/main.js
@@ -1,53 +1,70 @@
 let college = [
     {
         name: "Harry",
-        age: 18,
-        group: "ИС2-1",
-        sudID: 32232,
+        age: 17,
+        group: "ИС-2-1",
+        studID: 321232,
         typeLearning: "Очная",
         task: ["Физ-ра", "МДК-02-02", "МДК-02-02", "МДК-04-01"],
-        cash: 100,
+        cash: 0,
         isStudent: true
     },
-
     {
         name: "Ron",
-        age: 18,
-        group: "ССА-2-1",
-        sudID: 32232,
-        typeLearning: "Очная",
-        task: ["Физ-ра", "МДК-02-02", "МДК-02-02", "МДК-04-01"],
-        cash: 5500,
+        age: 19,
+        group: "ССА-3-2",
+        studID: 321232,
+        typeLearning: "Заочная",
+        task: ["ИЗО", "БЖ", "ПРАВО", "МДК-04-01"],
+        cash: 0,
         isStudent: true
     },
     {
-        name:"Tom",
+        name: "Tom",
         age: 32,
         isKurator: true,
-        kuratorGroup:"ССА-3-2",
+        kuratorGroup: "ССА-3-2",
         kafedra: "Инф.технологии",
-        predmet: ["МДК-02-02", "МДК-02-02", "МДК-04-01"],
+        predmet: ["МДК-02-1","МДК-02-2","МДК-09.01","МДК-09-02"],
+        isStudent: false,
+    },
+    {
+        name: "Bob",
+        age: 32,
+        isKurator: true,
+        kuratorGroup: "ССА-2-2",
+        kafedra: "Инф.технологии",
+        predmet: ["МДК-02-1","МДК-02-2","МДК-09.01","МДК-09-02"],
         isStudent: false,
     }
 ];
 
-if ( college[0]["isStudent"] === true ) {
-    console.log(`Вы выбрали студента ${college[0]["name"]}`);
-    if ( college[0]["age"] >= 18 ) {
-        console.log(`Студент ${college[0]["name"]} совершеннолетний. Ему ${college[0]["age"]}`);
+let count = 2;
+
+let personstdnt = college[count]["IsStudent"]
+let personName = college[count]["name"]
+let personAge = college[count]["age"]
+let personCash = college[count]["cash"]
+let personkrt = college[count]['isKurator']
+let collegeGroup = college[count]["kuratorGroup"]
+
+if ( personstdnt === true ) {
+    console.log(`Вы выбрали студента ${personName}`);
+    if ( personAge >= 18 ) {
+        console.log(`Студент ${personName} совершеннолетний. Ему ${personAge}`);
     } else {
-        console.log(`Студент ${college[0]["name"]} не совершеннолетний. Ему ${college[0]["age"]}`);
+        console.log(`Студент ${personName} не совершеннолетний. Ему ${personAge}`);
     }
-    if (college[0]["cash"] >= 4500 ) {
-        console.log(`Студент ${college[0]["name"]} получает повышенную стипендию. (${college[0]["cash"]} руб.)`);
-    } else if ( college[0]["cash"] <= 4400 && college[0]["cash"] >= 1) {
-        console.log(`Студент ${college[0]["name"]} получает обычную стипендию. (${college[0]["cash"]} руб.)`);
-    } else if  ( college[0]["cash"] === 0 ) {
-        console.log(`Студент ${college[0]["name"]} не получает стипендию.`);
+    if (personCash >= 4500 ) {
+        console.log(`Студент ${personName} получает повышенную стипендию. (${personCash} руб.)`);
+    } else if ( personCash <= 4400 && personCash >= 1) {
+        console.log(`Студент ${personName} получает обычную стипендию. (${personName} руб.)`);
+    } else if  ( personCash === 0 ) {
+        console.log(`Студент ${personName} не получает стипендию.`);
     }
-} else if ( college[0]["isStudent"] === false ) {
-    console.log(`Вы выбрали студента ${college[0]["name"]}`);
-    if ( college[0]["isKurator"] === true ) {
-        console.log(`Преподаватель ${college[0]["name"]} куратор группы ${college[0][kuratorGroup]} `);
+} else if ( personstdnt === false ) {
+    console.log(`Вы выбрали студента ${personName}`);
+    if ( personkrt === true ) {
+        console.log(`Преподаватель ${personName} куратор группы ${collegeGroup} `);
     }
 }
