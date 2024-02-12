99ca6f24852dcd41df830eda6390b0ed1fb9d417
kuramagod <raxutogame@gmail.com>
2024-02-12 10:17:41 +0300

    Работа над циклами

---
+++ task_4/main.js
@@ -0,0 +1,34 @@
+//const student = ["Tom", "Bob", "Harry"];
+//for (let i = 0; i < student.length; i++) {
+//    let result = student[i];
+//    console.log(result);
+//}
+
+
+//let people  = [ ["Tom", 18], ["Bob", 17], ["Stan", 17] ];
+//console.log(people);
+
+//for (let i = 0; i < people.length; i++) {
+//    for (let j = 0; j < people[i].length; j++) {
+//        let result = people[i][j]
+//        console.log(result)
+//    }
+//    console.log("_______");
+//}
+
+
+//const students = ["Tom", "Bob", "Harry", "Ron"];
+
+// in - индексы массива
+// of - элементы массива
+
+//for (let item in students) {
+//    console.log(students[item]);
+//}
+
+const person = {name:"Tom", age:23};
+
+
+for ( let item in person) {
+    console.log(person[item]); 
+}
